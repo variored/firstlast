e2478ec23ee7630573bfe066e3e410a53d01eb74
variowhite <variowhite@outlook.com>
2020-04-21 01:46:53 +0700

    edit: set new size of form4(library)

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -29,23 +29,23 @@
         private void InitializeComponent()
         {
             this.panelTotal = new System.Windows.Forms.Panel();
-            this.panelWords = new System.Windows.Forms.Panel();
-            this.labelWords = new System.Windows.Forms.Label();
-            this.panelFrameRight = new System.Windows.Forms.Panel();
-            this.panelFrameTop = new System.Windows.Forms.Panel();
-            this.panelTopLeft = new System.Windows.Forms.Panel();
             this.panelTitle = new System.Windows.Forms.Panel();
             this.labelTitle = new System.Windows.Forms.Label();
-            this.panelFramePictureLeft = new System.Windows.Forms.Panel();
-            this.panelFramePictureRight = new System.Windows.Forms.Panel();
-            this.panelFramePictureTop = new System.Windows.Forms.Panel();
-            this.panelFramePictureBottom = new System.Windows.Forms.Panel();
+            this.panelTopLeft = new System.Windows.Forms.Panel();
             this.pictureBoxWordsLogo = new System.Windows.Forms.PictureBox();
+            this.panelFramePictureBottom = new System.Windows.Forms.Panel();
+            this.panelFramePictureTop = new System.Windows.Forms.Panel();
+            this.panelFramePictureRight = new System.Windows.Forms.Panel();
+            this.panelFramePictureLeft = new System.Windows.Forms.Panel();
+            this.panelWords = new System.Windows.Forms.Panel();
+            this.labelWords = new System.Windows.Forms.Label();
+            this.panelFrameTop = new System.Windows.Forms.Panel();
+            this.panelFrameRight = new System.Windows.Forms.Panel();
             this.panelTotal.SuspendLayout();
-            this.panelWords.SuspendLayout();
-            this.panelTopLeft.SuspendLayout();
             this.panelTitle.SuspendLayout();
+            this.panelTopLeft.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWordsLogo)).BeginInit();
+            this.panelWords.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelTotal
@@ -56,61 +56,8 @@
             this.panelTotal.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelTotal.Location = new System.Drawing.Point(0, 0);
             this.panelTotal.Name = "panelTotal";
-            this.panelTotal.Size = new System.Drawing.Size(800, 451);
+            this.panelTotal.Size = new System.Drawing.Size(824, 451);
             this.panelTotal.TabIndex = 0;
-            // 
-            // panelWords
-            // 
-            this.panelWords.AutoScroll = true;
-            this.panelWords.Controls.Add(this.labelWords);
-            this.panelWords.Controls.Add(this.panelFrameTop);
-            this.panelWords.Controls.Add(this.panelFrameRight);
-            this.panelWords.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelWords.Location = new System.Drawing.Point(415, 0);
-            this.panelWords.Name = "panelWords";
-            this.panelWords.Size = new System.Drawing.Size(385, 451);
-            this.panelWords.TabIndex = 0;
-            // 
-            // labelWords
-            // 
-            this.labelWords.AutoSize = true;
-            this.labelWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelWords.ForeColor = System.Drawing.Color.White;
-            this.labelWords.Location = new System.Drawing.Point(3, 23);
-            this.labelWords.MaximumSize = new System.Drawing.Size(380, 0);
-            this.labelWords.Name = "labelWords";
-            this.labelWords.Size = new System.Drawing.Size(60, 24);
-            this.labelWords.TabIndex = 0;
-            this.labelWords.Text = "label1";
-            // 
-            // panelFrameRight
-            // 
-            this.panelFrameRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelFrameRight.Location = new System.Drawing.Point(364, 0);
-            this.panelFrameRight.Name = "panelFrameRight";
-            this.panelFrameRight.Size = new System.Drawing.Size(21, 451);
-            this.panelFrameRight.TabIndex = 1;
-            // 
-            // panelFrameTop
-            // 
-            this.panelFrameTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelFrameTop.Location = new System.Drawing.Point(0, 0);
-            this.panelFrameTop.Name = "panelFrameTop";
-            this.panelFrameTop.Size = new System.Drawing.Size(364, 20);
-            this.panelFrameTop.TabIndex = 3;
-            // 
-            // panelTopLeft
-            // 
-            this.panelTopLeft.Controls.Add(this.pictureBoxWordsLogo);
-            this.panelTopLeft.Controls.Add(this.panelFramePictureBottom);
-            this.panelTopLeft.Controls.Add(this.panelFramePictureTop);
-            this.panelTopLeft.Controls.Add(this.panelFramePictureRight);
-            this.panelTopLeft.Controls.Add(this.panelFramePictureLeft);
-            this.panelTopLeft.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTopLeft.Location = new System.Drawing.Point(0, 0);
-            this.panelTopLeft.Name = "panelTopLeft";
-            this.panelTopLeft.Size = new System.Drawing.Size(415, 258);
-            this.panelTopLeft.TabIndex = 1;
             // 
             // panelTitle
             // 
@@ -133,37 +80,18 @@
             this.labelTitle.Text = "label1";
             this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panelFramePictureLeft
+            // panelTopLeft
             // 
-            this.panelFramePictureLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelFramePictureLeft.Location = new System.Drawing.Point(0, 0);
-            this.panelFramePictureLeft.Name = "panelFramePictureLeft";
-            this.panelFramePictureLeft.Size = new System.Drawing.Size(145, 258);
-            this.panelFramePictureLeft.TabIndex = 1;
-            // 
-            // panelFramePictureRight
-            // 
-            this.panelFramePictureRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelFramePictureRight.Location = new System.Drawing.Point(270, 0);
-            this.panelFramePictureRight.Name = "panelFramePictureRight";
-            this.panelFramePictureRight.Size = new System.Drawing.Size(145, 258);
-            this.panelFramePictureRight.TabIndex = 2;
-            // 
-            // panelFramePictureTop
-            // 
-            this.panelFramePictureTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelFramePictureTop.Location = new System.Drawing.Point(145, 0);
-            this.panelFramePictureTop.Name = "panelFramePictureTop";
-            this.panelFramePictureTop.Size = new System.Drawing.Size(125, 66);
-            this.panelFramePictureTop.TabIndex = 3;
-            // 
-            // panelFramePictureBottom
-            // 
-            this.panelFramePictureBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelFramePictureBottom.Location = new System.Drawing.Point(145, 191);
-            this.panelFramePictureBottom.Name = "panelFramePictureBottom";
-            this.panelFramePictureBottom.Size = new System.Drawing.Size(125, 67);
-            this.panelFramePictureBottom.TabIndex = 3;
+            this.panelTopLeft.Controls.Add(this.pictureBoxWordsLogo);
+            this.panelTopLeft.Controls.Add(this.panelFramePictureBottom);
+            this.panelTopLeft.Controls.Add(this.panelFramePictureTop);
+            this.panelTopLeft.Controls.Add(this.panelFramePictureRight);
+            this.panelTopLeft.Controls.Add(this.panelFramePictureLeft);
+            this.panelTopLeft.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelTopLeft.Name = "panelTopLeft";
+            this.panelTopLeft.Size = new System.Drawing.Size(415, 258);
+            this.panelTopLeft.TabIndex = 1;
             // 
             // pictureBoxWordsLogo
             // 
@@ -176,24 +104,96 @@
             this.pictureBoxWordsLogo.TabIndex = 0;
             this.pictureBoxWordsLogo.TabStop = false;
             // 
+            // panelFramePictureBottom
+            // 
+            this.panelFramePictureBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelFramePictureBottom.Location = new System.Drawing.Point(145, 191);
+            this.panelFramePictureBottom.Name = "panelFramePictureBottom";
+            this.panelFramePictureBottom.Size = new System.Drawing.Size(125, 67);
+            this.panelFramePictureBottom.TabIndex = 3;
+            // 
+            // panelFramePictureTop
+            // 
+            this.panelFramePictureTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelFramePictureTop.Location = new System.Drawing.Point(145, 0);
+            this.panelFramePictureTop.Name = "panelFramePictureTop";
+            this.panelFramePictureTop.Size = new System.Drawing.Size(125, 66);
+            this.panelFramePictureTop.TabIndex = 3;
+            // 
+            // panelFramePictureRight
+            // 
+            this.panelFramePictureRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelFramePictureRight.Location = new System.Drawing.Point(270, 0);
+            this.panelFramePictureRight.Name = "panelFramePictureRight";
+            this.panelFramePictureRight.Size = new System.Drawing.Size(145, 258);
+            this.panelFramePictureRight.TabIndex = 2;
+            // 
+            // panelFramePictureLeft
+            // 
+            this.panelFramePictureLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelFramePictureLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelFramePictureLeft.Name = "panelFramePictureLeft";
+            this.panelFramePictureLeft.Size = new System.Drawing.Size(145, 258);
+            this.panelFramePictureLeft.TabIndex = 1;
+            // 
+            // panelWords
+            // 
+            this.panelWords.AutoScroll = true;
+            this.panelWords.Controls.Add(this.labelWords);
+            this.panelWords.Controls.Add(this.panelFrameTop);
+            this.panelWords.Controls.Add(this.panelFrameRight);
+            this.panelWords.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelWords.Location = new System.Drawing.Point(415, 0);
+            this.panelWords.Name = "panelWords";
+            this.panelWords.Size = new System.Drawing.Size(409, 451);
+            this.panelWords.TabIndex = 0;
+            // 
+            // labelWords
+            // 
+            this.labelWords.AutoSize = true;
+            this.labelWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWords.ForeColor = System.Drawing.Color.White;
+            this.labelWords.Location = new System.Drawing.Point(3, 23);
+            this.labelWords.MaximumSize = new System.Drawing.Size(380, 0);
+            this.labelWords.Name = "labelWords";
+            this.labelWords.Size = new System.Drawing.Size(60, 24);
+            this.labelWords.TabIndex = 0;
+            this.labelWords.Text = "label1";
+            // 
+            // panelFrameTop
+            // 
+            this.panelFrameTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelFrameTop.Location = new System.Drawing.Point(0, 0);
+            this.panelFrameTop.Name = "panelFrameTop";
+            this.panelFrameTop.Size = new System.Drawing.Size(388, 20);
+            this.panelFrameTop.TabIndex = 3;
+            // 
+            // panelFrameRight
+            // 
+            this.panelFrameRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelFrameRight.Location = new System.Drawing.Point(388, 0);
+            this.panelFrameRight.Name = "panelFrameRight";
+            this.panelFrameRight.Size = new System.Drawing.Size(21, 451);
+            this.panelFrameRight.TabIndex = 1;
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.ClientSize = new System.Drawing.Size(824, 451);
             this.Controls.Add(this.panelTotal);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(816, 99999);
-            this.MinimumSize = new System.Drawing.Size(400, 0);
+            this.MaximumSize = new System.Drawing.Size(840, 99999);
+            this.MinimumSize = new System.Drawing.Size(425, 39);
             this.Name = "Form4";
             this.Text = "FirstLast | Library";
             this.panelTotal.ResumeLayout(false);
+            this.panelTitle.ResumeLayout(false);
+            this.panelTopLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWordsLogo)).EndInit();
             this.panelWords.ResumeLayout(false);
             this.panelWords.PerformLayout();
-            this.panelTopLeft.ResumeLayout(false);
-            this.panelTitle.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWordsLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
