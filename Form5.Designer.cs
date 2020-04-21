b670ada6b2fa94884d9c60c30fb5d5ff56cf7fd9
variowhite <variowhite@outlook.com>
2020-04-22 01:48:15 +0700

    add: add border to form5

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -29,25 +29,26 @@
         private void InitializeComponent()
         {
             this.panelTotal = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelUsername = new System.Windows.Forms.Label();
             this.panelBottom = new System.Windows.Forms.Panel();
             this.buttonOk = new System.Windows.Forms.Button();
             this.panelWinner = new System.Windows.Forms.Panel();
-            this.panelTop = new System.Windows.Forms.Panel();
             this.labelWinner = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.labelUsername = new System.Windows.Forms.Label();
+            this.panelTop = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelTotal.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.panelBottom.SuspendLayout();
             this.panelWinner.SuspendLayout();
             this.panelTop.SuspendLayout();
-            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelTotal
             // 
             this.panelTotal.BackColor = System.Drawing.Color.White;
+            this.panelTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelTotal.Controls.Add(this.panel1);
             this.panelTotal.Controls.Add(this.panelBottom);
             this.panelTotal.Controls.Add(this.panelWinner);
@@ -58,13 +59,33 @@
             this.panelTotal.Size = new System.Drawing.Size(800, 450);
             this.panelTotal.TabIndex = 0;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelUsername);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 278);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(798, 30);
+            this.panel1.TabIndex = 3;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelUsername.Location = new System.Drawing.Point(0, 0);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(798, 30);
+            this.labelUsername.TabIndex = 0;
+            this.labelUsername.Text = "username";
+            this.labelUsername.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // panelBottom
             // 
             this.panelBottom.Controls.Add(this.buttonOk);
             this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelBottom.Location = new System.Drawing.Point(0, 402);
+            this.panelBottom.Location = new System.Drawing.Point(0, 400);
             this.panelBottom.Name = "panelBottom";
-            this.panelBottom.Size = new System.Drawing.Size(800, 48);
+            this.panelBottom.Size = new System.Drawing.Size(798, 48);
             this.panelBottom.TabIndex = 2;
             // 
             // buttonOk
@@ -88,18 +109,8 @@
             this.panelWinner.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelWinner.Location = new System.Drawing.Point(0, 194);
             this.panelWinner.Name = "panelWinner";
-            this.panelWinner.Size = new System.Drawing.Size(800, 84);
+            this.panelWinner.Size = new System.Drawing.Size(798, 84);
             this.panelWinner.TabIndex = 1;
-            // 
-            // panelTop
-            // 
-            this.panelTop.BackColor = System.Drawing.Color.SteelBlue;
-            this.panelTop.Controls.Add(this.pictureBox1);
-            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Location = new System.Drawing.Point(0, 0);
-            this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(800, 194);
-            this.panelTop.TabIndex = 0;
             // 
             // labelWinner
             // 
@@ -109,30 +120,20 @@
             this.labelWinner.ForeColor = System.Drawing.Color.Green;
             this.labelWinner.Location = new System.Drawing.Point(0, 0);
             this.labelWinner.Name = "labelWinner";
-            this.labelWinner.Size = new System.Drawing.Size(800, 84);
+            this.labelWinner.Size = new System.Drawing.Size(798, 84);
             this.labelWinner.TabIndex = 0;
             this.labelWinner.Text = "WINNER";
             this.labelWinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panel1
+            // panelTop
             // 
-            this.panel1.Controls.Add(this.labelUsername);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 278);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 30);
-            this.panel1.TabIndex = 3;
-            // 
-            // labelUsername
-            // 
-            this.labelUsername.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelUsername.Location = new System.Drawing.Point(0, 0);
-            this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(800, 30);
-            this.labelUsername.TabIndex = 0;
-            this.labelUsername.Text = "username";
-            this.labelUsername.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.panelTop.BackColor = System.Drawing.Color.SteelBlue;
+            this.panelTop.Controls.Add(this.pictureBox1);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(798, 194);
+            this.panelTop.TabIndex = 0;
             // 
             // pictureBox1
             // 
@@ -159,11 +160,11 @@
             this.Text = "Form5";
             this.TopMost = true;
             this.panelTotal.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.panelBottom.ResumeLayout(false);
             this.panelBottom.PerformLayout();
             this.panelWinner.ResumeLayout(false);
             this.panelTop.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
